5361b4946fd4c3f1821bff25c6dbcdf1339c5828
Edgar-M123 <edgar.ad.martinez@gmail.com>
2024-08-10 13:33:59 -0400

    change to pydantic to schema func

--- back-end/websocket_types.py
+++ back-end/websocket_types.py
@@ -45,6 +45,7 @@ def pydantic_to_schema(pydantic_dict: dict) -> genai_types.Schema:
         return pydantic_dict
     
     clean_dict = clean_schema(pydantic_dict=pydantic_dict)
+    print("clean_dict: ", clean_dict)
     new_schema = genai_types.Schema(mapping=clean_dict)
 
     return new_schema
